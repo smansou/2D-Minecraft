156b0ba0626e244f80b4e6d40175d644bd206634
Sobhi Mansour <sobhi@Sobhis-MBP.lan>
2022-04-30 17:55:52 +0300

    added cloud animation and some styling

--- logic/main.js
+++ logic/main.js
@@ -43,37 +43,38 @@ const blockTypes = {
     2: 'stone',
     3: 'wood',
     4: 'grass',
-    5: 'leaf'
-}
-const blocksAndTools = {
-    'axe':  ['wood'],
-    'shovel': ['dirt','grass'],
-    'pickaxe': ['stone'],
-    'shears': ['leaf']
+    5: 'leaf',
     
 }
+const blocksAndTools = {
+    'axe': ['wood'],
+    'shovel': ['dirt', 'grass'],
+    'pickaxe': ['stone'],
+    'shears': ['leaf']
 
-const gameObject ={
+}
+
+const gameObject = {
     selectedTool: '',
     selectedBlock: '',
     selectedResource: '',
     status: "building"
-  
+
 
 }
 
-   function updateInventory(){
+function updateInventory() {
     document.querySelector(".woodCount").innerHTML = inventory["wood"];
     document.querySelector(".stoneCount").innerHTML = inventory["stone"];
     document.querySelector(".dirtCount").innerHTML = inventory["dirt"];
     document.querySelector(".grassCount").innerHTML = inventory["grass"];
     document.querySelector(".leafCount").innerHTML = inventory["leaf"];
-   }
-    updateInventory();
-   document.querySelector(".game-board").addEventListener('click', ()=>{updateInventory()});
+}
+updateInventory();
+document.querySelector(".game-board").addEventListener('click', () => { updateInventory() });
 
 function start() {
-    
+
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             const block = document.createElement("div");
@@ -84,69 +85,75 @@ function start() {
     }
 
 
-    
+
     const allResources = document.querySelectorAll("[data-type='resource']");
     for (let elm = 0; elm < allResources.length; elm++) {
         allResources[elm].addEventListener('click', () => {
-            
-            gameObject.status = "building";
             gameObject.selectedResource = allResources[elm].getAttribute("class");
+            if (inventory[`${gameObject.selectedResource}`]>0){
+            gameObject.status = "building";
+            allTools.forEach((e) => { e.setAttribute("data-selected", "off") })
+            allResources.forEach((e) => { e.setAttribute("data-selected", "off") })
+            document.querySelector(`.${gameObject.selectedResource}`).setAttribute("data-selected", "on");
 
-})
+         }
+         })
+    
     }
-   
-const allTools = document.querySelectorAll("[data-type='tool']");
+
+
+    const allTools = document.querySelectorAll("[data-type='tool']");
     for (let elm = 0; elm < allTools.length; elm++) {
-        
+
+
+
         allTools[elm].addEventListener('click', () => {
-            allTools[0].setAttribute("data-selected", "off");
-            allTools[1].setAttribute("data-selected", "off");
-            allTools[2].setAttribute("data-selected", "off");
-            allTools[3].setAttribute("data-selected", "off");
-            
+
             gameObject.status = "collecting";
             gameObject.selectedTool = allTools[elm].getAttribute("data-toolType");
-            allTools[elm].setAttribute("data-selected", "on");
+            allTools.forEach((e) => { e.setAttribute("data-selected", "off") })
+            allResources.forEach((e) => { e.setAttribute("data-selected", "off") })
+            document.querySelector(`.${gameObject.selectedTool}`).setAttribute("data-selected", "on");
 
-            
-           
-})
+
+
+
+        })
     }
-    
-    
+
+
     const allBlocks = document.querySelectorAll("[data-type='block']");
     for (let elm = 0; elm < allBlocks.length; elm++) {
         allBlocks[elm].addEventListener('click', () => {
 
-                //! check if building or collecting/////////////////////////////
-                
-                if (gameObject.status === "collecting"){
-            gameObject.selectedBlock = allBlocks[elm].getAttribute("data-blockType");
-            // if(gameObject.selectedTool!=''){ 
-                if (blocksAndTools[gameObject.selectedTool][0]===gameObject.selectedBlock || blocksAndTools[gameObject.selectedTool][1]===gameObject.selectedBlock){
+            //! check if building or collecting/////////////////////////////
+
+            if (gameObject.status === "collecting") {
+                gameObject.selectedBlock = allBlocks[elm].getAttribute("data-blockType");
+
+                if (blocksAndTools[gameObject.selectedTool][0] === gameObject.selectedBlock || blocksAndTools[gameObject.selectedTool][1] === gameObject.selectedBlock) {
                     allBlocks[elm].setAttribute("data-blockType", "sky");
                     inventory[gameObject.selectedBlock]++;
-                    
+
                 }
-            // }
-        }  
-        if(gameObject.status === "building"){
-            //! check if block is empty(sky) ==> check if resource count in inventory is more than zero.==>place
-            if (allBlocks[elm].getAttribute("data-blockType") ==="sky"){
-                if (inventory[gameObject.selectedResource]>0) {
-                    allBlocks[elm].setAttribute("data-blockType",gameObject.selectedResource);
-                    inventory[gameObject.selectedResource]--;
+
+            }
+            if (gameObject.status === "building") {
+                //! check if block is empty(sky) ==> check if resource count in inventory is more than zero.==>place
+                if (allBlocks[elm].getAttribute("data-blockType") === "sky") {
+                    if (inventory[gameObject.selectedResource] > 0) {
+                        allBlocks[elm].setAttribute("data-blockType", gameObject.selectedResource);
+                        inventory[gameObject.selectedResource]--;
+                    }
                 }
             }
-        } 
         })
     }
 
-   
+
 
 }
-
-
+ 
 
 
 
